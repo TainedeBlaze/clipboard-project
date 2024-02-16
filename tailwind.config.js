936e154bea8f5aec0b6c8402ff164c4a38ef05eb
Taine <tainedb@gmail.com>
2024-02-15 21:30:56 -0700

    finished features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '974px',
+        'xl': '1440px',
+      },
+      fontFamily: {
+        'sans': ['"Bai Jamjuree"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
